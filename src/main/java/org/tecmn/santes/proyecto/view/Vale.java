aebf55c8afb684c6c4201f00e7e9016fac9d666c
SantesDLux <uwu.metroscat@gmail.com>
2025-05-06 03:57:43 -0600

    equisde

--- src/main/java/org/tecmn/santes/proyecto/view/Vale.java
+++ src/main/java/org/tecmn/santes/proyecto/view/Vale.java
@@ -26,18 +26,59 @@ public class Vale extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("campus Minatitlan");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 30, 820, 70);
+
+        jLabel3.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("INSTITUTO TECNOLOGICO DE MINATITLAN");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(0, 0, 820, 70);
+
+        jLabel2.setFont(new java.awt.Font("Felix Titling", 0, 24)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Buzon");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(570, 170, 260, 50);
+
+        jLabel4.setFont(new java.awt.Font("Felix Titling", 0, 24)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Vale");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 170, 260, 50);
+
+        jLabel5.setFont(new java.awt.Font("Felix Titling", 0, 24)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Datos");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(290, 170, 260, 50);
+
+        jButton1.setText("jButton1");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(620, 250, 150, 50);
+
+        jButton2.setText("jButton1");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(60, 250, 150, 50);
+
+        jButton3.setText("jButton1");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(350, 250, 150, 50);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +119,13 @@ public class Vale extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     // End of variables declaration//GEN-END:variables
 }
